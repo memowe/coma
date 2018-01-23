f9d36a07d2d764fd7a08ac70a10da735f29b612e
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2018-01-23 18:54:32 +0100

    Cleanup whitespace

--- coma.pl
+++ coma.pl
@@ -253,7 +253,7 @@ post '/delete_sure' => sub {
 app->types->type(tgf => 'text/plain;charset=UTF-8');
 get '/tgf_export' => sub {
     my $c = shift;
-    
+
     # force download
     my $fn = $c->stash('map')->name . '.tgf';
     $c->res->headers->content_disposition("attachment; filename=$fn");
