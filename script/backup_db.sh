88fab7121e611228fdef01c40a6b0396afa529e7
Mirko Westermeier <mirko@westermeier.de>
2018-08-11 22:40:55 +0200

    Cleanup

--- script/backup_db.sh
+++
@@ -1,2 +0,0 @@
-timestamp=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-sqlite3 data/graph.sqlite '.dump' > "data/backup/$timestamp.sql"
--- script/reset_db.sh
+++
@@ -1,3 +0,0 @@
-rm data/graph.sqlite
-sqlite3 data/graph.sqlite < data/schema.sql
-sqlite3 data/graph.sqlite < data/init_data.sql
